4eee07fda6ae08664e7670b3378f1532a9e16baf
Slava-k1852 <Slava-k1852@yandex.ru>
2024-09-03 14:56:38 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,6 @@
 def send_email(message, recipient,*,sender = "university.help@gmail.com"):
     if '@' not in recipient or not (recipient.endswith('.com') or recipient.endswith('.ru')
-     or recipient.endswith('.net')) or '@' not in sender or not (sender.endswith('.com') or sender.endswith('.ru')
+     or recipient.endswith('.net')) and '@' not in sender or not (sender.endswith('.com') or sender.endswith('.ru')
      or sender.endswith('.net')):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif recipient == sender:
